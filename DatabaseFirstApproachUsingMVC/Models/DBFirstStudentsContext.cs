e3183d46175eb9ab64a11253a50abab237f56e1e
uma-ram <jananijishumar@gmail.com>
2022-07-12 10:48:59 +0100

    Add project files.

---
+++ DatabaseFirstApproachUsingMVC/Models/DBFirstStudentsContext.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace DatabaseFirstApproachUsingMVC.Models
+{
+    public partial class DBFirstStudentsContext : DbContext
+    {
+        public DBFirstStudentsContext()
+        {
+        }
+
+        public DBFirstStudentsContext(DbContextOptions<DBFirstStudentsContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<City> Cities { get; set; } = null!;
+        public virtual DbSet<Student> Students { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB; Database=DBFirstStudents; Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<City>(entity =>
+            {
+                entity.Property(e => e.CityId).HasColumnName("CityID");
+
+                entity.Property(e => e.CityName).HasMaxLength(100);
+
+                entity.Property(e => e.Country).HasMaxLength(100);
+
+                entity.Property(e => e.State).HasMaxLength(100);
+            });
+
+            modelBuilder.Entity<Student>(entity =>
+            {
+                entity.Property(e => e.StudentId).HasColumnName("StudentID");
+
+                entity.Property(e => e.EmailAddress).HasMaxLength(150);
+
+                entity.Property(e => e.StudentName).HasMaxLength(50);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ DatabaseFirstApproachUsingMVC/Models/Student.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DatabaseFirstApproachUsingMVC.Models
+{
+    public partial class Student
+    {
+        public int StudentId { get; set; }
+        public string StudentName { get; set; } = null!;
+        public string? EmailAddress { get; set; }
+    }
+}
